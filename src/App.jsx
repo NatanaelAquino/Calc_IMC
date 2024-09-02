86f4298d6ef5d949d152607fcee82b093776befd
Natanael Aquino <natanael.aquino@tecno.com.br>
2024-09-02 09:24:36 -0300

    Projeot

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ImcCalc from "./components/ImcCalc";
+
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <p>Calculadora IMC</p>
+      <ImcCalc/>
+    </div>
+  );
+}
+
+export default App;
